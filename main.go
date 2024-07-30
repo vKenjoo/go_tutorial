b2b3b50e2256e824bfa1a892241d84bbe6af9cc2
Josh Kenn Viray <joshkennviray@gmail.com>
2024-07-30 11:12:44 +0800

    float and ints

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	var intNum int
+	var intNum int // since it was not defined, it is always equal to 0
 	fmt.Println(intNum)
 
-	/*
+	/* the key word int automatically sets it to either int32/int64 
+	(depende ong your architecture)
 	Signed Bits
 	int8 = 1 byte with a range of [-128 | 127]
 	int16 = 2 bytes with a range of [-32,768 | 32,767]
@@ -19,4 +20,9 @@ func main()  {
 	uint32 = 3 bytes with a range of [0 | 4,294,967,295]
 	uint64 = 4 bytes with a range of [0 | 18,446,744,073,709,551,615]
 	*/
+
+	var floatNum1 float32 = 12345678.9
+	var floatNum2 float64 = 12345678.9
+	fmt.Println("here is float32: ", floatNum1)
+	fmt.Println("here is float64: ", floatNum2)
 }
