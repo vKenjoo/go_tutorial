0478817b917ed5a3a3b5f52238e92d6bfa445534
Josh Kenn Viray <joshkennviray@gmail.com>
2024-07-30 11:05:24 +0800

    initial start

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var intNum int
+	fmt.Println(intNum)
+
+	/*
+	Signed Bits
+	int8 = 1 byte with a range of [-128 | 127]
+	int16 = 2 bytes with a range of [-32,768 | 32,767]
+	int32 = 3 bytes with a range of [-2,147,483,648 | 2,147,483,647]
+	int64 = 4 bytes with a range of [-9,223,372,036,854,775,808 | 9,223,372,036,854,775,807]
+	
+	Unsigned Bits (positive integers ONLY)
+	uint8 = 1 byte with a range of [0 | 255]
+	uint16 = 2 bytes with a range of [0 | 65,535]
+	uint32 = 3 bytes with a range of [0 | 4,294,967,295]
+	uint64 = 4 bytes with a range of [0 | 18,446,744,073,709,551,615]
+	*/
+}
